3256184a878c729fca90ef951c693b9a17bc0178
danaskr <sakerdana1@gmail.com>
2025-07-16 12:21:02 +0200

    Added backend Spring Boot project

---
+++ backend/cardflip/src/main/java/com/project/cardflip/CardflipApplication.java
@@ -0,0 +1,13 @@
+package com.project.cardflip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardflipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardflipApplication.class, args);
+	}
+
+}
---
+++ backend/cardflip/src/test/java/com/project/cardflip/CardflipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cardflip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardflipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
