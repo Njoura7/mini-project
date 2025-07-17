ea81016db98a0e8b161c2f9d20b295c9f14e2878
danaskr <sakerdana1@gmail.com>
2025-07-17 11:25:37 +0200

    fix : difficulty exception handeling

--- backend/cardflip/src/main/java/com/project/cardflip/exceptions/RestExceptionHandler.java
+++ backend/cardflip/src/main/java/com/project/cardflip/exceptions/RestExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.project.cardflip.exceptions;
 
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,6 +22,16 @@ public class RestExceptionHandler {
                     System.currentTimeMillis()) ,
                 exception.getStatus());
 
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidEnum(HttpMessageNotReadableException exception) {
+        ErrorResponse error = new ErrorResponse(
+                "Invalid input -> " + exception.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
